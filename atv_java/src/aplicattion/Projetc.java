5d49d55214294aec22f2141f5eeef7c54b281640
Luiz Gustavo Freitas de Araújo <112349592+Luizgustavo2308@users.noreply.github.com>
2023-06-03 10:28:09 -0300

    Add files via upload

---
+++ atv_java/src/aplicattion/Projetc.java
@@ -0,0 +1,40 @@
+package aplicattion;
+
+public class Projetc {
+    public static void main(String[] args) {
+
+        //1 questão
+        int largura=2000,largura1=2000, comp=2000, comp2=2000, adição;
+        adição=largura+largura1+comp+comp2;
+
+        System.out.println("o perimetro do terreno igual a "+ adição+" metros");
+
+        //2 questao
+
+    int idade= 15, dia=365, multiplica;
+    multiplica =idade*dia;
+
+        System.out.println("minha idade em dias e "+multiplica);
+
+        //3 questão
+
+        String nome="Gustavo";
+        int idade1= 16;
+        float alt=1.64f;
+        System.out.println("meu nome e " +nome+ " minha idade e " + idade1+ " e tenho " +alt + " de altura");
+
+        //4 questao
+
+        int salario=1500, horas=8,multiplicacao;
+
+        multiplicacao=salario*horas;
+
+        System.out.println("meu salario em horas e " +multiplicacao);
+
+        //5 questao
+
+        float altura= 1.64f,peso=76,div;
+        div=peso/altura;
+        System.out.println("meu imc e "+div);
+    }
+}
